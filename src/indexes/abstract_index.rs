35533a7e983547e92cc3e1432d94469398c14f53
Anay Bhakat <38054625+abhakat@users.noreply.github.com>
2024-11-16 20:39:03 -0600

    bit of stuff

--- src/indexes/abstract_index.rs
+++ src/indexes/abstract_index.rs
@@ -1,6 +1,11 @@
 use crate::types::{KeyT, ValT};
 
 pub trait Index {
-    fn insert(&mut self, key: KeyT, val: ValT) -> ();
+    fn insert(&mut self, key: KeyT, val: ValT);
     fn get(&self, key: &KeyT) -> Option<&ValT>;
+    fn remove(&mut self, key: &KeyT) -> Option<ValT>;
+    fn contains_key(&self, key: &KeyT) -> bool;
+    fn iter(&self) -> Box<dyn Iterator<Item = (&KeyT, &ValT)>>;
+    fn clear(&mut self);
+    fn len(&self) -> usize;
 }
