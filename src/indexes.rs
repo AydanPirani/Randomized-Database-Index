8aa99a1570ba1c34e84ff3f950451ce952f6c26c
Aydan Pirani <aydanpirani@gmail.com>
2024-11-18 11:37:21 -0600

    More testing framework

--- src/indexes.rs
+++ src/indexes.rs
@@ -2,3 +2,4 @@ pub mod abstract_index;
 pub mod hashmap_index;
 pub mod scapegoat_index;
 pub mod skiplist_index;
+pub mod splaytree_index;
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ mod logger;
 mod types;
 
 use executor::SequenceExecutor;
-use indexes::hashmap_index::{HashMapIndex};
+use indexes::hashmap_index::HashMapIndex;
+use indexes::scapegoat_index::ScapegoatIndex;
 use indexes::skiplist_index::SkipListIndex;
+use indexes::splaytree_index::SplayTreeIndex;
 use logger::Logger;
 
 use std::env;
@@ -26,10 +28,16 @@ fn main() {
     let mut executor = SequenceExecutor::new(logger);
 
     let hashmap = Box::new(HashMapIndex::new());
-    let skiplist = Box::new(SkipListIndex::new());
-
     executor.add_index(hashmap, "hashmap");
+
+    let skiplist = Box::new(SkipListIndex::new());
     executor.add_index(skiplist, "skiplist");
+
+    // let scapegoat = Box::new(ScapegoatIndex::new());
+    // executor.add_index(scapegoat, "scapegoat");
+
+    // let splaytree = Box::new(SplayTreeIndex::new());
+    // executor.add_index(splaytree, "splaytree");
 
     match executor.execute(&args[1], &args[2]) {
         Ok(_) => {
