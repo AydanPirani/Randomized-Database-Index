fb41a0dc97963771f7d2deb6c71d50051b27122b
Aydan Pirani <aydanpirani@gmail.com>
2024-11-24 14:22:27 -0600

    Added a working runner

--- scripts/runner.py
+++ scripts/runner.py
@@ -5,7 +5,6 @@ import postprocessing
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-
 operation_cts = [100, 1000, 10000]
 write_ratios = [0.1, 0.5, 0.9]
 cycle_sizes = [5, 50, 500]
@@ -13,11 +12,10 @@ duplicate_counts = [5, 20, 50]
 keys = [1, 50, 5000]
 
 
-
 def random_workload(sequence_dir, output_dir, figure_dir):
     for operation_ct in operation_cts:
         for write_ratio in write_ratios:
-            name = f"{int(write_ratio*100)}-{operation_ct}-randomized"
+            name = f"{int(write_ratio*100)}write-{operation_ct}ops-randomized"
 
             sequence_file = f"{sequence_dir}/{name}.seq"
             output_file = f"{output_dir}/{name}.csv"
@@ -26,16 +24,75 @@ def random_workload(sequence_dir, output_dir, figure_dir):
             runner = workloads.WorkloadGenerator(output_file=sequence_file, total_operations=operation_ct)
             runner.generate_workload(write_ratio)
             del runner;
-            print(os.getcwd())
+
+            
             command = ["cargo", "run", sequence_file, output_file]
-            print(" ".join(command))
             subprocess.run(command)
             plt = postprocessing.get_plot(output_file, name)
             plt.savefig(image_file)
 
+
+def sequential_workload(sequence_dir, output_dir, figure_dir):
+    for operation_ct in operation_cts:
+            name = f"{operation_ct}ops-sequential"
+
+            sequence_file = f"{sequence_dir}/{name}.seq"
+            output_file = f"{output_dir}/{name}.csv"
+            image_file = f"{figure_dir}/{name}.png"
+
+            runner = workloads.WorkloadGenerator(output_file=sequence_file, total_operations=operation_ct)
+            runner.generate_sequential_workload()
+            del runner;
+
+            command = ["cargo", "run", sequence_file, output_file]
+            subprocess.run(command)
+            plt = postprocessing.get_plot(output_file, name)
+            plt.savefig(image_file)
+
+def cyclic_workload(sequence_dir, output_dir, figure_dir):
+    for operation_ct in operation_cts:
+        for write_ratio in write_ratios:
+            for cycle_size in cycle_sizes:
+                name = f"{int(write_ratio*100)}write-{cycle_size}cycle-{operation_ct}ops-cyclic"
+
+                sequence_file = f"{sequence_dir}/{name}.seq"
+                output_file = f"{output_dir}/{name}.csv"
+                image_file = f"{figure_dir}/{name}.png"
+
+                runner = workloads.WorkloadGenerator(output_file=sequence_file, total_operations=operation_ct)
+                runner.generate_cyclic_workload(cycle_size=cycle_size, write_ratio=write_ratio)
+                del runner;
                 
+                command = ["cargo", "run", sequence_file, output_file]
+                subprocess.run(command)
+                plt = postprocessing.get_plot(output_file, name)
+                plt.savefig(image_file)
+
+def repeated_workload(sequence_dir, output_dir, figure_dir):
+    for operation_ct in operation_cts:
+        for write_ratio in write_ratios:
+            for duplicate_ct in duplicate_counts:
+                name = f"{int(write_ratio*100)}write-{duplicate_ct}dups-{operation_ct}ops-repeated"
+
+                sequence_file = f"{sequence_dir}/{name}.seq"
+                output_file = f"{output_dir}/{name}.csv"
+                image_file = f"{figure_dir}/{name}.png"
+
+                runner = workloads.WorkloadGenerator(output_file=sequence_file, total_operations=operation_ct)
+                runner.generate_cyclic_workload(duplicate_ct=duplicate_ct, write_ratio=write_ratio)
+                del runner;
+                
+                command = ["cargo", "run", sequence_file, output_file]
+                subprocess.run(command)
+                plt = postprocessing.get_plot(output_file, name)
+                plt.savefig(image_file)
+
+
 def run_workloads(sequence_dir, output_dir, figure_dir):
     random_workload(sequence_dir, output_dir, figure_dir)
+    sequential_workload(sequence_dir, output_dir, figure_dir)
+    cyclic_workload(sequence_dir, output_dir, figure_dir)
+    repeated_workload(sequence_dir, output_dir, figure_dir)
 
 if __name__ == "__main__":
     root = os.getenv('RDI_ROOT')
