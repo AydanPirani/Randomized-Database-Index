4962088d965c92a97fd343d768d8b340ac74db0f
divinedab <divyack2@illinois.edu>
2024-11-23 22:55:21 -0800

    added deletes into treap logic

--- src/indexes/treap_index.rs
+++ src/indexes/treap_index.rs
@@ -47,18 +47,35 @@ impl TreapIndex {
 
 impl Index for TreapIndex {
     fn insert(&mut self, key: KeyT, val: ValT) {
-        self.index.insert(Element::new(key, Pair { priority: 0, value: val}))
+        if let Some(element) = self.index.get(key)  {
+            // println!("** INSERT --> already exists :(");
+            self.index.delete(&key);
+            self.index.insert(Element::new(key, Pair { priority: 0, value: val}))
+        }
+        else {
+            // println!("** INSERT --> new insert :)");
+            self.index.insert(Element::new(key, Pair { priority: 0, value: val}))
+        }
+        
     }
 
     fn get(&mut self, key: &KeyT) -> Option<&ValT> {
+        // println!();
+        // println!("------------ READ ------------");
+        let size = self.index.size();
+        // println!("-- TREAP SIZE so far: {size} --");
         if let Some(element) = self.index.get(*key)  {
+            // println!("-- Key found! Key={key} -- ");
             let value = element.priority().value;
             let new_priority = element.priority().priority + 1;
+            self.index.delete(key);
             self.index.insert(Element::new(*key, Pair { priority: new_priority+1, value: value}));
         }  else {
+            // println!("-- Key NOT found! Key={key} --");
+            // println!();
             return None;
         }    
-
+        // println!();
         return Some(&(self.index.get(*key)?.priority().value));
     }
 
